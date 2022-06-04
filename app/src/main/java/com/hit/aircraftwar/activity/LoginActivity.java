3bf28beebf426193189635a5869b0c1308cc98ab
haha <935877636@qq.com>
2022-06-04 20:39:24 +0800

    实现账户的获取

--- app/src/main/java/com/hit/aircraftwar/activity/LoginActivity.java
+++ app/src/main/java/com/hit/aircraftwar/activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.hit.aircraftwar.R;
 
@@ -23,7 +24,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    //获取游戏账户和密码，启动游戏等
     private void gameLunch(){
+        EditText usernameText = (EditText)findViewById(R.id.usernameText);
+        EditText passwordText = (EditText)findViewById(R.id.passwordText);
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
